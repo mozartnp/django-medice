7c46c04ddf1952e8cd519cad52d5b8345ac79736
Regis Santos <regis42santos@gmail.com>
2022-04-11 20:38:37 -0300

    Usa o LoginView normal do Django junto com o template_name específico.
    * Não usa AuthLogin da View, mas usa o LoginView do django.contrib.auth

--- backend/custom_user/urls.py
+++ backend/custom_user/urls.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from .views import AuthLogin, AuthSignup
+from .views import AuthSignup
 
 app_name = 'custom_user'
 
 urlpatterns = [
-    path('login/', AuthLogin.as_view(), name='login'),
+    path('login/', LoginView.as_view(template_name='custom_user/login.html'), name='login'),
     path('signup/', AuthSignup.as_view(), name='signup'),
 ]
--- backend/custom_user/views.py
+++ backend/custom_user/views.py
@@ -1,20 +1,15 @@
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView
 
 from backend.custom_user.forms import SignupForm
-from backend.custom_user.models import CustomUser
+from backend.custom_user.models import User
 
 
 # TODO a CBV aqui seria createview, uma vez que vou criar uma item no model?
 # TODO Como eu testo um CBV de forma certa, o que preciso olhar?
 # TODO ter mais de um success_url
 class AuthSignup(CreateView):
-    model = CustomUser
-    form_class = SignupForm
+    model = User
     template_name = "custom_user/signup.html"
+    form_class = SignupForm
     success_url = reverse_lazy('core:index')
-
-
-class AuthLogin(LoginView):
-    template_name = "custom_user/login.html"
