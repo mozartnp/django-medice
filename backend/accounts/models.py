fc6473a4d36514579c60c46a4b5a6c68c343c47f
Regis Santos <regis42santos@gmail.com>
2022-04-16 22:12:53 -0300

    Implementa envio de emails. close #3

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.mail import send_mail
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -79,3 +80,18 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('backend.core.urls', namespace='core')),
-    path('', include('backend.accounts.urls', namespace='accounts')),
+    path('accounts/', include('backend.accounts.urls')),  # sem namespace
     path('', include('backend.patient.urls', namespace='patient')),
     path('admin/', admin.site.urls),
 ]
